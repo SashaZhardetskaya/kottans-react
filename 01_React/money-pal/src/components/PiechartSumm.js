1f5ff2c5a3c11de60eb38aa8955dc0be5d87e60d
SashaZhardetskaya <sasha310209@gmail.com>
2017-12-03 22:40:36 +0200

    finished filter for incomes/expenses

--- 01_React/money-pal/src/components/PiechartSumm.js
+++ 01_React/money-pal/src/components/PiechartSumm.js
@@ -22,71 +22,20 @@ export default class PiechartSumm extends Component {
 
     const accounts = this.props.data;
 
-    const incomeAccounts = accounts.filter(x => x.amount > 0);
-    const expensesAccounts = accounts.filter(x => x.amount < 0);
-
-    // const reducedIncomeAccounts = incomeAccounts.reduce((acc, item) => {
-    //   acc.value = item.amount += item.amount;
-    //   let sum =
-    //   acc = {
-    //     value: item.amount
-    //   };
-    //   console.log(acc);
-    //   return acc;
-    //
-    // }, []);
-    // console.log(reducedIncomeAccounts);
-
-    // const reducedIncomeAccounts = incomeAccounts.reduce((prev, curr) => {
-    //   prev += curr.amount;
-    //   return prev;
-    // }, []);
-
-
-    // const reducedIncomeAccounts = incomeAccounts.reduce((acc, obj) => {
-    //   return acc + obj.amount;
-    // }, 0);
-
-
-    // const reducedIncomeAccounts = incomeAccounts.reduce((acc, obj) => {
-    //   return {
-    //     value: acc.value + obj.amount
-    //   }
-    // }, {value: 0});
-
-    const reducedIncomeAccounts = incomeAccounts.reduce((acc, obj) => {
-      if (obj.amount > 0) {
+    const incomeSumm = (accounts.filter(x => x.amount > 0)).reduce((acc, obj) => {
         return { value: acc.value + obj.amount, label: 'incomes' };
-      }
     }, {value: 0});
 
-    console.log(reducedIncomeAccounts);
+    const expensesSumm = (accounts.filter(x => x.amount < 0)).reduce((acc, obj) => {
+      return { value: acc.value - obj.amount, label: 'expenses' };
+    }, {value: 0});
 
+    console.log(incomeSumm);
+    console.log(expensesSumm);
 
+    const dataSumm = [incomeSumm, expensesSumm];
 
-    const reducedAccountsArr = accounts.reduce((acc, item) => {
-      let accByCategory = acc.find(x => x.category === item.category);
-      if(accByCategory) {
-        accByCategory.amount += item.amount;
-      } else {
-        acc.push({
-          amount: item.amount,
-          category: item.category
-        });
-      }
-      return acc;
-    }, []);
-
-    const data1 = incomeAccounts.map((obj) => {
-      obj.value = obj.amount; //the name of thw column must be 'value' for plugin
-      return obj;
-    });
-
-    const data2 = expensesAccounts.map((obj) => {
-      obj.value = obj.amount; //the name of thw column must be 'value' for plugin
-      return obj;
-    });
-
+    console.log(dataSumm);
 
 
 
@@ -98,36 +47,27 @@ export default class PiechartSumm extends Component {
       <div>
         <PieChart
           className = "pie-charts"
-          data={ data1 }
+          data={ dataSumm }
           expandedSector={expandedSector}
           onSectorHover={this.handleMouseEnterOnSector}
           sectorStrokeWidth={2}
           expandOnHover
           shrinkOnTouchEnd
         />
-        {/*<div>*/}
-          {/*{*/}
-            {/*data.map((element, i) => (*/}
-              {/*<div key={i} >*/}
-                {/*/!*<span style={{background: element.color}}></span>*!/*/}
-                {/*<span style={{fontWeight: this.state.expandedSector === i ? "bold" : null}}>*/}
-                  {/*{element.category} : {element.amount}*/}
-                {/*</span>*/}
-              {/*</div>*/}
-            {/*))*/}
-          {/*}*/}
-        {/*</div>*/}
+        <div>
+          {
+            dataSumm.map((element, i) => (
+              <div key={i} >
+                {/*<span style={{background: element.color}}></span>*/}
+                <span style={{fontWeight: this.state.expandedSector === i ? "bold" : null}}>
+                  {element.label} : {element.value}
+                </span>
+              </div>
+            ))
+          }
+        </div>
 
 
-        <PieChart
-          className = "pie-charts"
-          data={ data2 }
-          expandedSector={expandedSector}
-          onSectorHover={this.handleMouseEnterOnSector}
-          sectorStrokeWidth={2}
-          expandOnHover
-          shrinkOnTouchEnd
-        />
       </div>
     )
   }
